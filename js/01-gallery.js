4788e7ee93aa0a02dda46e9cd5aa2555f7a1aa1d
Vol4enak <52600927+Vol4enak@users.noreply.github.com>
2022-12-10 02:53:39 +0200

    123

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,54 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+
+const imageMarcup = createGalleryItems(galleryItems);
+
+gallery.insertAdjacentHTML("beforeend", imageMarcup);
+
+function createGalleryItems(images) {
+  return images
+    .map(({ original, preview, description }) => {
+      return `
+<div class="gallery__item">
+  <a class="gallery__link" href="${original}" target="_self" >
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`;
+    })
+    .join("");
+}
+
+function onClickImage(evt) {
+  evt.preventDefault();
+  if (evt.target.nodeName !== "IMG") {
+    return;
+  }
+  const instance = basicLightbox.create(
+    `<img
+      class="gallery__image"
+      src="${evt.target.dataset.source}"
+      alt="${evt.target.alt}"
+    />`
+  );
+  instance.show();
+}
+
+gallery.addEventListener("click", onClickImage);
+
+// <div class="gallery__item">
+//   <a class="gallery__link" href="large-image.jpg">
+//     <img
+//       class="gallery__image"
+//       src="small-image.jpg"
+//       data-source="large-image.jpg"
+//       alt="Image description"
+//     />
+//   </a>
+// </div>; */}
